5a44f484413bf14c717852af46bbed34e192e01b
Behruz <behruz@bictory.io>
2024-10-26 12:39:10 +0500

    deploy 1.9

--- server.js
+++ server.js
@@ -82,12 +82,6 @@ app.delete('/api/workers/:id', (req, res) => {
   res.status(204).send();
 });
 
-// Static files and server start
-app.use(express.static(path.join(__dirname, 'dist')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
 // Start the server
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://0.0.0.0:${PORT}`);
